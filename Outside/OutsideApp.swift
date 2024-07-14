40527155855696665749af8ed6cb7deb7938fcfd
Leny <leny.levant95@icloud.com>
2024-07-14 02:53:26 +0200

    Initial Commit

---
+++ Outside/OutsideApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutsideApp.swift
+//  Outside
+//
+//  Created by Leny Levant on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct OutsideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OutsideTests/OutsideTests.swift
@@ -0,0 +1,17 @@
+//
+//  OutsideTests.swift
+//  OutsideTests
+//
+//  Created by Leny Levant on 14/07/2024.
+//
+
+import Testing
+@testable import Outside
+
+struct OutsideTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
